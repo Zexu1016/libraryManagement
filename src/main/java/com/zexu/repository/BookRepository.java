c96eafa4c92ae16c34ef42ddfa7ef7776e99195a
Zexu3366 <Zexu3366.163.com>
2023-05-16 22:16:46 +0800

    完成了数据层的基础crud操作

--- src/main/java/com/zexu/repository/BookRepository.java
+++ src/main/java/com/zexu/repository/BookRepository.java
@@ -1,13 +1,30 @@
 package com.zexu.repository;
 
 import com.zexu.entity.Book;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
 public interface BookRepository {
     @Select("select * from tb_book where id = #{id}")
-    public Book getById(Integer id);
+    Book selectById(Integer id);
 
-    public void save(Book book);
+    @Select("select * from tb_book")
+    List<Book> selectAll();
+
+    @Select("select * from tb_book limit #{start},#{number}")
+    List<Book> selectPage(Integer start, Integer number);
+
+    @Insert("insert into tb_book (type, name, description) VALUE (#{type}, #{name}, #{description})")
+    void insert(Book book);
+
+    @Delete("delete from tb_book where id = #{id}")
+    void delete(Integer id);
+
+    @Update("update tb_book set type = #{type}, name = #{name}, description = #{description} where id = #{id}")
+    void update(Book book);
+
+
+
 }
--- src/test/java/com/zexu/repositoryTest/BookRepositoryTest.java
+++ src/test/java/com/zexu/repositoryTest/BookRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.zexu.repositoryTest;
 
+import com.zexu.entity.Book;
 import com.zexu.repository.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +11,41 @@ public class BookRepositoryTest {
 
     @Autowired
     private BookRepository bookRepository;
+
     @Test
     void selectByIdTest() {
-        System.out.println(bookRepository.getById(5));
+        System.out.println(bookRepository.selectById(5));
     }
+
+    @Test
+    void selectAllTest() {
+        System.out.println(bookRepository.selectAll());
+    }
+    @Test
+    void insertTest() {
+        Book book = new Book();
+        book.setName("最新出炉！");
+        book.setDescription("测试一下能不能成功插入");
+        book.setType("测试");
+        bookRepository.insert(book);
+        System.out.println(bookRepository.selectAll());
+    }
+    @Test
+    void deleteTest() {
+        bookRepository.delete(8);
+        System.out.println(bookRepository.selectAll());
+    }
+    @Test
+    void updateTest() {
+        Book book = new Book();
+        book.setId(8);
+        book.setDescription("测试一下能不能成功更新");
+        bookRepository.update(book);
+        System.out.println(bookRepository.selectAll());
+    }
+    @Test
+    void selectPageTest() {
+        System.out.println(bookRepository.selectPage(1,3));
+    }
+
 }
