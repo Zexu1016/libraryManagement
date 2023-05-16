ca77518f6b89c1a42c40e5c15c4d459657640ab7
Zexu3366 <Zexu3366.163.com>
2023-05-16 23:07:32 +0800

    完成表现层类创建

---
+++ src/main/java/com/zexu/controller/BookController.java
@@ -0,0 +1,15 @@
+package com.zexu.controller;
+
+import com.zexu.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    private final BookService bookService;
+
+
+}
