8b3ee390ca8d53d358ba1c03a5d299714b981fbb
Zexu3366 <Zexu3366.163.com>
2023-05-16 22:35:19 +0800

    初步完成了业务层

---
+++ src/main/java/com/zexu/service/BookService.java
@@ -0,0 +1,15 @@
+package com.zexu.service;
+
+import com.zexu.entity.Book;
+
+import java.util.List;
+
+public interface BookService {
+    Boolean save(Book book);
+    Boolean delete(Integer id);
+    Boolean update(Book book);
+
+    Book selectById(Integer id);
+
+    List<Book> selectAll();
+}
---
+++ src/main/java/com/zexu/service/impl/BookServiceImpl.java
@@ -0,0 +1,44 @@
+package com.zexu.service.impl;
+
+import com.zexu.entity.Book;
+import com.zexu.repository.BookRepository;
+import com.zexu.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository bookRepository;
+
+    @Override
+    public Boolean save(Book book) {
+        bookRepository.insert(book);
+        return true;
+    }
+
+    @Override
+    public Boolean delete(Integer id) {
+        bookRepository.delete(id);
+        return true;
+    }
+
+    @Override
+    public Boolean update(Book book) {
+        bookRepository.update(book);
+        return true;
+    }
+
+    @Override
+    public Book selectById(Integer id) {
+        return bookRepository.selectById(id);
+    }
+
+    @Override
+    public List<Book> selectAll() {
+        return bookRepository.selectAll();
+    }
+}
