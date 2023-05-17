59cc8c1df51cb46dc19134fee3bd1b14a59d864f
Zexu3366 <Zexu3366.163.com>
2023-05-17 11:31:35 +0800

    初步完成表现层编写

--- src/main/java/com/zexu/controller/BookController.java
+++ src/main/java/com/zexu/controller/BookController.java
@@ -1,15 +1,47 @@
 package com.zexu.controller;
 
+import com.zexu.entity.Book;
 import com.zexu.service.BookService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/books")
 public class BookController {
     private final BookService bookService;
+
+    @GetMapping("/get")
+    public List<Book> getAll() {
+        return bookService.selectAll();
+    }
+
+    @GetMapping("/get/{id}")
+    public Book getById(@PathVariable Integer id) {
+        return bookService.selectById(id);
+    }
+
+    @GetMapping("/delete/{id}")
+    public Boolean delete(@PathVariable Integer id) {
+        bookService.delete(id);
+        return true;
+    }
+
+    @PostMapping("/save")
+    public Boolean save(@RequestBody Book book) {
+        bookService.save(book);
+        return true;
+    }
+
+    @PostMapping("/update")
+    public Boolean update(@RequestBody Book book) {
+        bookService.update(book);
+        return true;
+    }
+
+
 
 
 }
