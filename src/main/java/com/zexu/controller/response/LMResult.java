1359b5e19a4fcd8f58ee7812441a68115c02ab58
Zexu3366 <Zexu3366.163.com>
2023-05-17 22:59:49 +0800

    略微修改了协议组成

--- src/main/java/com/zexu/controller/response/LMResult.java
+++ src/main/java/com/zexu/controller/response/LMResult.java
@@ -1,5 +1,8 @@
 package com.zexu.controller.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record LMResult<T>(int statusCode, String message, T data) {
 
     public static <T> LMResult<T> success(T data) {return success(null, data);}
